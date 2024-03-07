9eca435145592563e1f98d337426aedc3e2941ef
leavesnight <leavesnight@126.com>
2024-03-07 21:03:29 +0800

    comment EGL destruction because of crashes

--- components/pango_windowing/src/display_x11.cpp
+++ components/pango_windowing/src/display_x11.cpp
@@ -244,10 +244,10 @@ X11GlContext::X11GlContext(std::shared_ptr<X11Display>& xdisplay) : display(xdis
 X11GlContext::~X11GlContext()
 {
     // cleanup EGL
-    eglMakeCurrent(egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-    if(egl_context) eglDestroyContext(egl_display, egl_context);
-    if(egl_surface) eglDestroySurface(egl_display, egl_surface);
-    if(egl_display) eglTerminate(egl_display);
+//    eglMakeCurrent(egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+//    if(egl_context) eglDestroyContext(egl_display, egl_context);
+//    if(egl_surface) eglDestroySurface(egl_display, egl_surface);
+//    if(egl_display) eglTerminate(egl_display);
 }
 
 X11Window::X11Window(
